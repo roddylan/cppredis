0645f68aa6096608a472ca4134d67f19a8ffc692
Roderick Lan <rodericklan03@gmail.com>
2025-02-13 15:06:48 -0700

    server starter code

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,1 +1,72 @@
 // server.cpp
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <netdb.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    // Flush after every std::cout / std::cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "Failed to create server socket\n";
+        return 1;
+    }
+
+    // ensures that we don't run into 'Address already in use' errors
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+        0) {
+        std::cerr << "setsockopt failed\n";
+        return 1;
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // redis uses 6379, use 6380 for now
+    // server_addr.sin_port = htons(6379);
+    server_addr.sin_port = htons(6380);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
+        0) {
+        std::cerr << "Failed to bind to port 6379\n";
+        return 1;
+    }
+
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        std::cerr << "listen failed\n";
+        return 1;
+    }
+
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    std::cout << "Waiting for a client to connect...\n";
+
+    // debug log
+    std::cout << "Logs appear here!\n";
+
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                           (socklen_t *)&client_addr_len);
+    std::cout << "Client connected\n";
+
+    char buffer[1024] = {0};
+    std::string RESPONSE{"+PONG\r\n"};
+    while (recv(client_fd, buffer, sizeof(buffer), 0)) {
+        send(client_fd, RESPONSE.c_str(), RESPONSE.size(), 0);
+    }
+
+    close(client_fd);
+    close(server_fd);
+
+    return 0;
+}
